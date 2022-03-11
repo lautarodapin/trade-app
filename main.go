d2dffa2b99e4157cfd2910128b0b357a85aefc35
lautarodapin <lautarodapin@gmail.com>
2022-03-11 20:09:15 -0300

    Hardcoded pair lists and users

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 	"trade-app/models"
 	"trade-app/schemas"
 
@@ -16,5 +17,23 @@ func main() {
 			"message": "pong",
 		})
 	})
+	pairList := []string{
+		"BTCUSDT",
+		"ETHUSDT",
+		"BNBUSDT",
+		"BCCUSDT",
+		"NEOUSDT",
+		"LTCUSDT",
+		"QTUMUSDT",
+		"ADAUSDT",
+		"XRPUSDT",
+		"EOSUSDT",
+	}
+	favPairList := pairList[:3]
+	users := [3]models.User{
+		{Id: 1, FirstName: "John", LastName: "Doe", Email: "user1@test.com", Token: "d73e:9666:2dec:2ed8:073f:7f52:1ffc:5b9d", Password: "password"},
+		{Id: 2, FirstName: "Koko", LastName: "Doe", Email: "user2@test.com", Token: "9ae4:9c47:a59f:9427:bc36:f6ec:536f:3c83", Password: "password"},
+		{Id: 3, FirstName: "Francis", LastName: "Sunday", Email: "user3@test.com", Token: "f049:fc4e:eb2a:2d50:2962:5ab7:f5c7:6b96", Password: "password"},
+	}
 	r.Run()
 }
