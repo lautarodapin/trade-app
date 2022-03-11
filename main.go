ebd3b44c9816b6ff1245cf27ca0cda4579eb5731
lautarodapin <lautarodapin@gmail.com>
2022-03-11 20:09:48 -0300

    Add users endpoints and login

--- main.go
+++ main.go
@@ -35,5 +35,52 @@ func main() {
 		{Id: 2, FirstName: "Koko", LastName: "Doe", Email: "user2@test.com", Token: "9ae4:9c47:a59f:9427:bc36:f6ec:536f:3c83", Password: "password"},
 		{Id: 3, FirstName: "Francis", LastName: "Sunday", Email: "user3@test.com", Token: "f049:fc4e:eb2a:2d50:2962:5ab7:f5c7:6b96", Password: "password"},
 	}
+
+	users_route := r.Group("/users")
+	{
+		users_route.POST("/login", func(c *gin.Context) {
+			var userLogin schemas.UserLogin
+			err := c.BindJSON(&userLogin)
+			if err != nil {
+				c.JSON(http.StatusBadRequest, gin.H{"detail": err.Error()})
+				return
+			}
+			for _, user := range users {
+				if user.Email == userLogin.Email && user.Password == userLogin.Password {
+					c.JSON(http.StatusOK, gin.H{"token": user.Token})
+					return
+				} else if user.Email == userLogin.Email && user.Password != userLogin.Password {
+					c.JSON(http.StatusUnauthorized, gin.H{"detail": "Invalid password"})
+					return
+				}
+			}
+			c.JSON(http.StatusNotFound, gin.H{"detail": fmt.Sprintf("User %s not found", userLogin.Email)})
+		})
+		users_route.GET("/current", func(c *gin.Context) {
+			var authHeader schemas.AuthHeader
+			err := c.BindHeader(&authHeader)
+			if err != nil {
+				c.JSON(http.StatusForbidden, gin.H{"detail": "No Authorization header"})
+				return
+			}
+			for _, user := range users {
+				if fmt.Sprintf("Bearer %s", user.Token) == authHeader.Authorization {
+					c.JSON(http.StatusOK, gin.H{"user": user})
+					return
+				}
+			}
+			c.JSON(http.StatusUnauthorized, gin.H{"detail": "Invalid token"})
+		})
+		users_route.GET("/:id", func(c *gin.Context) {
+			id, _ := strconv.Atoi(c.Param("id"))
+			for _, user := range users {
+				if user.Id == id {
+					c.JSON(http.StatusOK, user)
+					return
+				}
+			}
+			c.JSON(http.StatusNotFound, gin.H{"detail": fmt.Sprintf("User with id %d not found", id)})
+		})
+	}
 	r.Run()
 }
