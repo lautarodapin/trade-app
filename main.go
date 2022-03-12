45ce816f894ef0ee8221e2b67aabd4d21feb93e3
lautarodapin <lautarodapin@gmail.com>
2022-03-12 16:19:27 -0300

    Add sqlite database and switch csv to db and added middleware

--- main.go
+++ main.go
@@ -1,20 +1,20 @@
 package main
 
 import (
-	"encoding/csv"
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"os"
 	"strconv"
 	"strings"
 	"trade-app/models"
 	"trade-app/schemas"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
-func CORSMiddleware(users []models.User) gin.HandlerFunc {
+func CORSMiddleware(db *gorm.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
 
 		c.Header("Access-Control-Allow-Origin", "*")
@@ -22,91 +22,78 @@ func CORSMiddleware(users []models.User) gin.HandlerFunc {
 		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
 
-		token := c.Request.Header.Get("Authorization")
+		authHeader := c.Request.Header.Get("Authorization")
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
 			return
 		}
-		var _user models.User
-		for _, u := range users {
-			if u.Token == strings.Split(token, "Bearer ")[1] {
-				_user = u
-				break
-			}
+		fmt.Printf("authHeader: %s\n", authHeader)
+		if authHeader == "" {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"detail": "unauthorized"})
+			return
 		}
-		fmt.Printf("Usuario: %s\n", _user.Email)
+		var _user models.User
+		token := strings.TrimPrefix(authHeader, "Bearer ")
+		db.Where("token = ?", token).First(&_user)
+		fmt.Printf("token: %s\n", token)
+		fmt.Printf("_user: %+v\n", _user)
 		c.Set("user", _user)
-
 		c.Next()
 	}
 }
-
-func readFile(path string) ([][]string, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		return [][]string{}, err
-	}
-	defer file.Close()
-
-	reader := csv.NewReader(file)
-	records, err := reader.ReadAll()
-	if err != nil {
-		return [][]string{}, err
-	}
-
-	return records, nil
-}
-
 func main() {
+
+	db, _ := gorm.Open(sqlite.Open("gorm.sqlite"), &gorm.Config{})
+	models.AutoMigrate(db)
+	models.InitUsers(db)
+	models.InitPairList(db)
+	models.InitFavPairList(db)
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	public := r.Group("")
+	private := r.Group("")
+	private.Use(CORSMiddleware(db))
+	public.GET("/ping", func(c *gin.Context) {
+		user, exists := c.Get("user")
+		if exists {
+			fmt.Printf("user: %+v\n", user)
+			c.JSON(200, gin.H{
+				"message": "pong",
+				"user":    user,
+			})
+			return
+		}
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"message": "ping",
 		})
 	})
-	var records [][]string
-	records, _ = readFile("./pair-list.csv")
-	pairList := []string{}
-	for _, v := range records {
-		pairList = append(pairList, v[0])
-	}
-	records, _ = readFile("./users.csv")
-	users := []models.User{}
-	for _, v := range records {
-		id, _ := strconv.Atoi(v[0])
-		user := models.User{
-			Id:        id,
-			FirstName: v[1],
-			LastName:  v[2],
-			Email:     v[3],
-			Token:     v[4],
-			Password:  v[5],
-		}
-		users = append(users, user)
-	}
-	favPairList := pairList[:3]
-	r.Use(CORSMiddleware(users))
+	var pairList []models.Pair
+	db.Find(&pairList)
 
-	users_route := r.Group("/users")
+	favPairList := pairList[:3]
+	var users []models.User
+	db.Find(&users)
+	public.POST("/login", func(c *gin.Context) {
+		var userLogin schemas.UserLogin
+		fmt.Printf("userLogin: %+v\n", userLogin)
+		err := c.BindJSON(&userLogin)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"detail": err.Error()})
+			return
+		}
+		var user models.User
+		db.Where("email = ?", userLogin.Email).First(&user)
+		if user.Email == userLogin.Email && user.Password == userLogin.Password {
+			c.JSON(http.StatusOK, gin.H{"token": user.Token})
+			return
+		} else if user.Email == userLogin.Email && user.Password != userLogin.Password {
+			c.JSON(http.StatusUnauthorized, gin.H{"detail": "Invalid password"})
+			return
+		}
+		c.JSON(http.StatusNotFound, gin.H{"detail": fmt.Sprintf("User %s not found", userLogin.Email)})
+	})
+
+	users_route := private.Group("/users")
 	{
-		users_route.POST("/login", func(c *gin.Context) {
-			var userLogin schemas.UserLogin
-			err := c.BindJSON(&userLogin)
-			if err != nil {
-				c.JSON(http.StatusBadRequest, gin.H{"detail": err.Error()})
-				return
-			}
-			for _, user := range users {
-				if user.Email == userLogin.Email && user.Password == userLogin.Password {
-					c.JSON(http.StatusOK, gin.H{"token": user.Token})
-					return
-				} else if user.Email == userLogin.Email && user.Password != userLogin.Password {
-					c.JSON(http.StatusUnauthorized, gin.H{"detail": "Invalid password"})
-					return
-				}
-			}
-			c.JSON(http.StatusNotFound, gin.H{"detail": fmt.Sprintf("User %s not found", userLogin.Email)})
-		})
 		users_route.GET("/current", func(c *gin.Context) {
 			var authHeader schemas.AuthHeader
 			err := c.BindHeader(&authHeader)
@@ -114,18 +101,17 @@ func main() {
 				c.JSON(http.StatusForbidden, gin.H{"detail": "No Authorization header"})
 				return
 			}
-			for _, user := range users {
-				if fmt.Sprintf("Bearer %s", user.Token) == authHeader.Authorization {
-					c.JSON(http.StatusOK, gin.H{"user": user})
-					return
-				}
+			user, exists := c.Get("user")
+			if exists {
+				c.JSON(http.StatusOK, gin.H{"user": user})
+				return
 			}
 			c.JSON(http.StatusUnauthorized, gin.H{"detail": "Invalid token"})
 		})
 		users_route.GET("/:id", func(c *gin.Context) {
 			id, _ := strconv.Atoi(c.Param("id"))
 			for _, user := range users {
-				if user.Id == id {
+				if user.ID == uint(id) {
 					c.JSON(http.StatusOK, user)
 					return
 				}
@@ -133,7 +119,7 @@ func main() {
 			c.JSON(http.StatusNotFound, gin.H{"detail": fmt.Sprintf("User with id %d not found", id)})
 		})
 	}
-	pair_list_route := r.Group("/pair-list")
+	pair_list_route := private.Group("/pair-list")
 	{
 		pair_list_route.GET("/", func(c *gin.Context) {
 			c.JSON(http.StatusOK, pairList)
@@ -147,12 +133,12 @@ func main() {
 			c.BindJSON(&jsonData)
 			symbol := jsonData.Symbol
 			for _, favPair := range favPairList {
-				if favPair == symbol {
+				if favPair.Symbol == symbol {
 					c.JSON(http.StatusConflict, gin.H{"detail": "Symbol already exists"})
 					return
 				}
 			}
-			favPairList = append(favPairList, symbol)
+			// favPairList = append(favPairList, symbol)
 			if len(favPairList) > 3 {
 				favPairList = favPairList[1:]
 			}
--- models/models.go
+++ models/models.go
@@ -1,10 +1,81 @@
 package models
 
+import (
+	"gorm.io/gorm"
+)
+
 type User struct {
-	Id        int    `json:"id"`
-	FirstName string `json:"firstName"`
-	LastName  string `json:"lastName"`
-	Email     string `json:"email"`
-	Token     string `json:"token"`
-	Password  string `json:"password"`
+	gorm.Model
+	FirstName string    `json:"firstName"`
+	LastName  string    `json:"lastName"`
+	Email     string    `json:"email" gorm:"unique;not null"`
+	Token     string    `json:"token"`
+	Password  string    `json:"password"`
+	FavPairs  []FavPair `json:"favPairs"`
+}
+
+type Pair struct {
+	gorm.Model
+	Symbol   string    `json:"symbol" gorm:"unique;not null"`
+	FavPairs []FavPair `json:"favPairs"`
+}
+
+type FavPair struct {
+	gorm.Model
+	UserID uint `json:"userId" gorm:"not null"`
+	PairID uint `json:"pairId" gorm:"not null"`
+}
+
+func AutoMigrate(db *gorm.DB) {
+	db.AutoMigrate(&User{})
+	db.AutoMigrate(&Pair{})
+	db.AutoMigrate(&FavPair{})
+}
+
+func InitUsers(db *gorm.DB) {
+	// The email unique constraint will prevent from recreating the same users
+	for _, user := range initialUsers {
+		db.Create(&user)
+	}
+}
+
+var initialUsers []User = []User{
+	{FirstName: "John", LastName: "Doe", Email: "user1@test.com", Token: "d73e:9666:2dec:2ed8:073f:7f52:1ffc:5b9d", Password: "password"},
+	{FirstName: "Koko", LastName: "Doe", Email: "user2@test.com", Token: "9ae4:9c47:a59f:9427:bc36:f6ec:536f:3c83", Password: "password"},
+	{FirstName: "Francis", LastName: "Sunday", Email: "user3@test.com", Token: "f049:fc4e:eb2a:2d50:2962:5ab7:f5c7:6b96", Password: "password"},
+}
+var initialPartList []string = []string{
+	"BTCUSDT",
+	"ETHUSDT",
+	"BNBUSDT",
+	"BCCUSDT",
+	"NEOUSDT",
+	"LTCUSDT",
+	"QTUMUSDT",
+	"ADAUSDT",
+	"XRPUSDT",
+	"EOSUSDT",
+}
+
+func InitPairList(db *gorm.DB) {
+	db.Unscoped().Session(&gorm.Session{AllowGlobalUpdate: true}).Delete(&Pair{})
+	for _, pair := range initialPartList {
+		db.Create(&Pair{Symbol: pair})
+	}
+
+}
+
+func InitFavPairList(db *gorm.DB) {
+	var users []User
+	var favPairs []FavPair
+	db.Find(&users)
+	db.Unscoped().Session(&gorm.Session{AllowGlobalUpdate: true}).Delete(&FavPair{})
+	for _, user := range users {
+		db.Find(&favPairs).Where("user_id = ?", user.ID)
+		for _, initialPair := range initialPartList[:3] {
+			var pair Pair
+			db.Where("symbol = ?", initialPair).First(&pair)
+			db.Create(&FavPair{UserID: user.ID, PairID: pair.ID})
+		}
+	}
 }
