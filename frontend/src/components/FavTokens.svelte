c010d7f8e8ff64de30683f8eefdea6388d3076c2
lautarodapin <lautarodapin@gmail.com>
2022-03-17 19:39:06 -0300

    Add prices ticker for fav symbols

--- frontend/src/components/FavTokens.svelte
+++ frontend/src/components/FavTokens.svelte
@@ -3,8 +3,32 @@
     import { favSymbols, fetchFavSymbols, token } from "../stores/index";
     import type { ApiResponse } from "../types";
 
+    let prices: { symbol: string; price: string }[] = [];
+
+    const getFavPrices = async () => {
+        if ($favSymbols.length === 0) return;
+        const response = await fetch(
+            "http://localhost:8080/pair-list/fav/prices",
+            {
+                headers: {
+                    Authorization: `Bearer ${$token}`,
+                },
+            }
+        );
+        const { data, status, message }: ApiResponse = await response.json();
+        if (status === "success") {
+            prices = data;
+        } else {
+            alert(message);
+        }
+    };
+
     onMount(() => {
+        const interval = setInterval(() => {
+            getFavPrices();
+        }, 5 * 1000);
         fetchFavSymbols();
+        return () => clearInterval(interval);
     });
 
     const removeFromFav = async (id: number) => {
@@ -27,18 +51,23 @@
     };
 </script>
 
-<div>
+<div class="m-10">
+    <h1 class="">Fav symbols</h1>
     <ul>
         {#each $favSymbols as favSymbol}
-            <li>
+            <li class="my-2">
                 {favSymbol.pair.symbol}
                 <button
+                    class="ml-2 rounded-sm py-1 px-2"
                     on:click={() => {
                         removeFromFav(favSymbol.ID);
                     }}
                 >
                     Remove from fav {favSymbol.ID}
                 </button>
+                Price: {prices.find(
+                    ({ symbol }) => symbol === favSymbol.pair.symbol
+                )?.price || "N/A"}
             </li>
         {/each}
     </ul>
