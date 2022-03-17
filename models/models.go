8caa1d9a5b587140870f17961afb49fc09e2b0a6
lautarodapin <lautarodapin@gmail.com>
2022-03-16 21:18:51 -0300

    Add hashed password

--- models/models.go
+++ models/models.go
@@ -55,9 +55,9 @@ func InitUsers(db *gorm.DB) {
 }
 
 var initialUsers []User = []User{
-	{FirstName: "John", LastName: "Doe", Email: "user1@test.com", Token: "d73e:9666:2dec:2ed8:073f:7f52:1ffc:5b9d", Password: "password"},
-	{FirstName: "Koko", LastName: "Doe", Email: "user2@test.com", Token: "9ae4:9c47:a59f:9427:bc36:f6ec:536f:3c83", Password: "password"},
-	{FirstName: "Francis", LastName: "Sunday", Email: "user3@test.com", Token: "f049:fc4e:eb2a:2d50:2962:5ab7:f5c7:6b96", Password: "password"},
+	{FirstName: "John", LastName: "Doe", Email: "user1@test.com", Token: "d73e:9666:2dec:2ed8:073f:7f52:1ffc:5b9d", Password: HashPassword("password")},
+	{FirstName: "Koko", LastName: "Doe", Email: "user2@test.com", Token: "9ae4:9c47:a59f:9427:bc36:f6ec:536f:3c83", Password: HashPassword("password")},
+	{FirstName: "Francis", LastName: "Sunday", Email: "user3@test.com", Token: "f049:fc4e:eb2a:2d50:2962:5ab7:f5c7:6b96", Password: HashPassword("password")},
 }
 var initialPartList []string = []string{
 	"BTCUSDT",
