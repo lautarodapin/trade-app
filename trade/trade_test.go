5b49836c34e657e2bb335564ebd4aa068385421c
lautarodapin <lautarodapin@gmail.com>
2022-03-18 20:48:56 -0300

    Wrap test

--- trade/trade_test.go
+++ trade/trade_test.go
@@ -29,27 +29,32 @@ func TestMakeTrade(t *testing.T) {
 	const SYMBOL = "FAKESYMBOL"
 	db.Create(&models.Pair{Symbol: SYMBOL})
 
-	t.Run("Buy 12 at 100", func(t *testing.T) {
+	t.Run("Buy 12 at 100 of FAKESYMBOL", func(t *testing.T) {
 		trade := createBuyTrade(db, user, 100, 12, SYMBOL)
 		if trade.ID == 0 {
 			t.Fatal("trade not created")
 		}
 	})
 
-	t.Run("Buy 17 at 99", func(t *testing.T) {
+	t.Run("Buy 17 at 99 of FAKESYMBOL", func(t *testing.T) {
 		trade := createBuyTrade(db, user, 99, 17, SYMBOL)
 		if trade.ID == 0 {
 			t.Fatal("trade not created")
 		}
 	})
 
-	t.Run("Buy 3 at 103", func(t *testing.T) {
+	t.Run("Buy 3 at 103 of FAKESYMBOL", func(t *testing.T) {
 		trade := createBuyTrade(db, user, 103, 3, SYMBOL)
 		if trade.ID == 0 {
 			t.Fatal("trade not created")
 		}
 	})
-	createBuyTrade(db, user, 103, 3, "OTHER")
+	t.Run("Buy 3 at 103 of OTHER symbol", func(t *testing.T) {
+		trade := createBuyTrade(db, user, 103, 3, "OTHER")
+		if trade.ID == 0 {
+			t.Fatal("trade not created")
+		}
+	})
 
 	t.Run("Total 32 at cost 3192", func(t *testing.T) {
 		var quantity, total float64
