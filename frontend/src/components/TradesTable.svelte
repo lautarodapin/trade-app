2eae430cba3c3514acb79dcf14ba6e0d03e9a699
lautarodapin <lautarodapin@gmail.com>
2022-03-18 23:57:35 -0300

    Add current earns

--- frontend/src/components/TradesTable.svelte
+++ frontend/src/components/TradesTable.svelte
@@ -1,23 +1,46 @@
 <script lang="ts">
     import { onMount } from "svelte";
-    import { fetchTrades, trades } from "../stores/index";
-    import { TradeType } from "../types";
+    import { fetchTrades, token, trades } from "../stores/index";
+    import { TradeType, type ApiResponse } from "../types";
 
+    let unrealized_pl: number = 0;
+    let net_pnl: number = 0;
+    let cumulative_pl = 0;
+
+    const getEarns = async () => {
+        const response = await fetch("http://localhost:8080/trades/earns", {
+            headers: { Authorization: `Bearer ${$token}` },
+        });
+        const { data, status, message }: ApiResponse = await response.json();
+        if (status === "success") {
+            cumulative_pl = data.cumulative_pl;
+            net_pnl = data.net_pnl;
+            unrealized_pl = data.unrealized_pl;
+        } else {
+            alert(message);
+        }
+    };
     onMount(() => {
-        fetchTrades();
+        fetchTrades().then(getEarns);
+        let timer = setInterval(() => {
+            getEarns();
+        }, 1000 * 15);
+        return () => clearInterval(timer);
     });
 </script>
 
-<div>
+<div class="inline-flex">
     <table class="m-4">
-        <tr>
-            <td class="px-4">ID</td>
-            <td class="px-4">Symbol</td>
-            <td class="px-4">Type</td>
-            <td class="px-4">Quantity</td>
-            <td class="px-4">Price</td>
-            <td class="px-4">Earns</td>
-        </tr>
+        <thead>
+            <tr>
+                <th class="px-4">ID</th>
+                <th class="px-4">Symbol</th>
+                <th class="px-4">Type</th>
+                <th class="px-4">Quantity</th>
+                <th class="px-4">Price</th>
+                <th class="px-4">Earns</th>
+            </tr>
+        </thead>
         <tbody>
             {#each $trades as trade}
                 <tr>
@@ -26,12 +49,32 @@
                     <td class="text-center my-2"
                         >{trade.type === TradeType.BUY ? "Buy" : "Sell"}</td
                     >
-                    <td class="text-center my-2">{trade.quantity.toFixed(4)}</td
-                    >
-                    <td class="text-center my-2">{trade.price.toFixed(4)}</td>
-                    <td class="text-center my-2">{trade.earns.toFixed(4)}</td>
+                    <td class="text-center my-2">{trade.quantity}</td>
+                    <td class="text-center my-2">{trade.price}</td>
+                    <td class="text-center my-2">{trade.earns}</td>
                 </tr>
             {/each}
+        </tbody>
+    </table>
+</div>
+<div class="inline-flex">
+    <table>
+        <thead>
+            <th colspan="2">Earns</th>
+        </thead>
+        <tbody>
+            <tr>
+                <th>Unrealized PL</th>
+                <td>{unrealized_pl}</td>
+            </tr>
+            <tr>
+                <th>Cumulative PL</th>
+                <td>{cumulative_pl}</td>
+            </tr>
+            <tr>
+                <th>Net PNL</th>
+                <td>{net_pnl}</td>
+            </tr>
         </tbody>
     </table>
 </div>
