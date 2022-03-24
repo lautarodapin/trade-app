857d4c46aacb02855f410254493df35085bc6260
lautarodapin <lautarodapin@gmail.com>
2022-03-24 11:44:37 -0300

    Move middlewares back to main

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 	"trade-app/models"
 	"trade-app/schemas"
 	"trade-app/symbols"
@@ -13,6 +14,51 @@ import (
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
+func AuthMiddleware(db *gorm.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		c.Header("Access-Control-Allow-Origin", "*")
+
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
+
+		authHeader := c.Request.Header.Get("Authorization")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		fmt.Printf("authHeader: %s\n", authHeader)
+		if authHeader == "" {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, schemas.Response{Status: "error", Message: "unauthorized"})
+			return
+		}
+		var _user models.User
+		token := strings.TrimPrefix(authHeader, "Bearer ")
+		db.Debug().Where("token = ?", token).First(&_user)
+		fmt.Printf("token: %s\n", token)
+		fmt.Printf("_user: %+v\n", _user)
+		c.Set("user", _user)
+		c.Next()
+	}
+}
 
 func main() {
 
